186ec1b79977be8706afa32417a6a6391acef990
pnguyen712 <122504431+pnguyen712@users.noreply.github.com>
2023-03-27 15:27:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<h1> Hello World! </h1>
+<h1> Thong tin sinh vien </h1><br>
+<h3> Ho ten: Pham Nguyen </h3><br>
+<h3> MSSV: B1809490 </h3><br>
+<h3> Dia chi: Ke Sach - Soc Trang </h3><br>
+<h3> Email: nguyenb1809490@student.ctu.edu.vn </h3><br>
+<h3> SDT: 0396600186 </h3>
