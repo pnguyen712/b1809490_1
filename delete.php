0e318fb78c2b1b1bc74ebca95f8928a503e134f8
pnguyen712 <122504431+pnguyen712@users.noreply.github.com>
2023-04-03 13:58:53 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,31 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<body>
+Insert Employee
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" class="border">
+    ID<BR> <input type="text" name="id" value="<?php echo $id;?>"><BR>
+    <input type="submit" name="submit" value="submit">
+    </form>
+<?php
+    $id=$_POST["id"];
+    $MYSQL_ADDON_HOST=getenv('MYSQL_ADDON_HOST');
+    $MYSQL_ADDON_PORT=getenv('MYSQL_ADDON_PORT');
+    $MYSQL_ADDON_BD=getenv('MYSQL_ADDON_DB');
+    $MYSQL_ADDON_USER=getenv('MYSQL_ADDON_USER');
+    $MYSQL_ADDON_PASSWORD=getenv('MYSQL_ADDON_PASSWORD');
+    $conn = mysqli_connect($MYSQL_ADDON_HOST,$MYSQL_ADDON_PORT,$MYSQL_ADDON_BD,$MYSQL_ADDON_USER,$MYSQL_ADDON_PASSWORD);
+    if(!$conn){
+        echo "ERROR : Unable to open database\n";
+    } else {
+        echo "Open database successfully\n";
+    }
+    $sql = "DELETE FROM users WHERE id=$id";
+    if (mysqli_query($conn,$sql)) {
+        echo "User deleted successfully.";
+    } else {
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+?>
+</body>
+</html>
