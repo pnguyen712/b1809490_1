ed42ed6f19e19ef6a371bfb5080a9e1d5295edec
pnguyen712 <122504431+pnguyen712@users.noreply.github.com>
2023-04-03 15:11:59 +0700

    Update create.php

--- create.php
+++ create.php
@@ -9,23 +9,26 @@ $MYSQL_ADDON_URI=getenv('mysql://usj4ncmyqyyx4dmp:yLWLmm89XwPxVdw8HzTY@btnvudazz
 
 echo "$MYSQL_ADDON_URI";
 
-//Kết nối cơ sở dữ liệu MySQL
 $conn = mysqli_connect($MYSQL_ADDON_HOST, $MYSQL_ADDON_USER, $MYSQL_ADDON_PASSWORD, $MYSQL_ADDON_BD);
-//Kiểm tra kết nối
-if($conn === false) {
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+mysqli_select_db($conn, $MYSQL_ADDON_BD);
+
+if (!$conn){
+    die("ERROR: Could not connect to MySQL. " . mysqli_connect_error());
 }
-//Hàm tạo bảng
+else {
+echo "\nConnection was successful<br>";
+}
+
 $sql = "CREATE TABLE users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(70) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
-    )";
+)";
+
 if (mysqli_query($conn, $sql)) {
     echo "Table created successfully.";
-} else {
+}else {
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
 }
 ?>
-
